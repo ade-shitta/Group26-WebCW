870962468deb3d9fcb292cbda49407f063ff3171
ade-shitta <119895344+ade-shitta@users.noreply.github.com>
2025-01-17 02:25:54 +0000

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -92,17 +92,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'your_db_name',
-        'USER': 'your_db_user',
-        'PASSWORD': 'your_db_password',
-        'HOST': 'your_db_host',
-        'PORT': '5432',
-        'TEST': {
-            'NAME': 'test_db',  # Temporary test database
-        },
-    }
+    'default': database.config()
 }
 
 
